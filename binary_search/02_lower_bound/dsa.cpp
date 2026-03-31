60b6d9aad383e03d1225339e52b5051660052bf8
Aryan Kumar <aryanempire168@gmail.com>
2026-03-31 09:29:08 +0530

    Add files via upload

---
+++ binary_search/02_lower_bound/dsa.cpp
@@ -0,0 +1,24 @@
+// nums[i] >= target
+class Solution{
+public:
+    int lowerBound(vector<int> &nums, int x){
+        int n = nums.size();
+        int ans = n;
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid]>=x){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+
+
+        }
+        return ans;
+        
+    }
+};

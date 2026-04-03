0a3100c3a0a35c4a1362450106d6132d223b09c3
Aryan Kumar <aryanempire168@gmail.com>
2026-04-03 20:54:23 +0530

    Add files via upload

---
+++ linked_list/15_remove_Nth_node_from_end/dsa.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        if(head == NULL || head->next == NULL){
+            if(n == 1){
+                return NULL;
+            }
+        }
+
+        for(int i = 0; i <n ; i++){
+            fast = fast->next;
+        }  
+        if(fast == NULL){
+            return head->next;
+        }     
+        while(fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode* remove = slow->next;
+        slow->next = slow->next->next;
+        delete remove ;
+        return head;
+        
+    }
+};
